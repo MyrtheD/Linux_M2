dbf5fe0ba1bcc6f291dca8ba2684e05d12a597d1
Myrthe <113888157+MyrtheD@users.noreply.github.com>
2023-12-09 18:34:03 +0100

    Add files via upload

--- linuxproject-m2/mdanfastapi/app/main.py
+++ linuxproject-m2/mdanfastapi/app/main.py
@@ -1,45 +1,7 @@
-from typing import List
-from fastapi import Depends, FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import Column, String
-import socket
+from fastapi import FastAPI
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost/mydatabase"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-class Fullname(Base):
-    __tablename__ = "studentname"
-    name = Column(String, primary_key=True)
-
-Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.get("/user")
-async def read_name(db:Session = Depends(get_db)):
-     users = db.query(Fullname).all()
-     return users
-
-# @app.get("/user")
-# async def read_user():
-#    return {"name": "Myrthe"}
-
-@app.get("/container_id")
-async def read_container_id():
-    return {"container_id": socket.gethostname()}
-
-
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
